53f6f381e66b1636eb228c53047f927ca9e64472
Melvin A <melvinalmonte@gmail.com>
2022-02-22 10:33:10 -0500

    Changed flex type for different browser sizes

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -2,81 +2,80 @@ import React from "react";
 import { chakra, Box, Image, Flex, useColorModeValue } from "@chakra-ui/react";
 
 const Card = ({ title, src }) => {
+  const repeatItem = [...Array(2).keys()];
   return (
     <Flex
       bg={useColorModeValue("#F9FAFB", "gray.600")}
       p={5}
+      pt={{ base: 5, md: 1 }}
       w="full"
       alignItems="center"
       justifyContent="center"
       className="card-container"
+      direction={{ base: "column", md: "row" }}
     >
-      <Box
-        maxW="xs"
-        mx="auto"
-        bg={useColorModeValue("white", "gray.800")}
-        shadow="lg"
-        rounded="lg"
-      >
-        <Box px={4} py={2}>
-          <chakra.h1
-            color={useColorModeValue("gray.800", "white")}
-            fontWeight="bold"
-            fontSize="3xl"
-            textTransform="uppercase"
-          >
-            {title}
-          </chakra.h1>
-          <chakra.p
-            mt={1}
-            fontSize="sm"
-            color={useColorModeValue("gray.600", "gray.400")}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos
-            quidem sequi illum facere recusandae voluptatibus
-          </chakra.p>
-        </Box>
-
-        <Image
-          h={40}
-          w="full"
-          fit="cover"
-          mt={2}
-          src={src}
-          alt="NIKE AIR"
-        />
-
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          px={4}
-          py={2}
-          bg="gray.900"
-          roundedBottom="lg"
+      {repeatItem.map(() => (
+        <Box
+          maxW="xs"
+          mx="auto"
+          bg={useColorModeValue("white", "gray.800")}
+          shadow="lg"
+          rounded="lg"
+          mt={{ base: 5 }}
         >
-          <chakra.h1 color="white" fontWeight="bold" fontSize="lg">
-            Some description
-          </chakra.h1>
-          <chakra.button
-            px={2}
-            py={1}
-            bg="white"
-            fontSize="xs"
-            color="gray.900"
-            fontWeight="bold"
-            rounded="lg"
-            textTransform="uppercase"
-            _hover={{
-              bg: "gray.200",
-            }}
-            _focus={{
-              bg: "gray.400",
-            }}
+          <Box px={4} py={2}>
+            <chakra.h1
+              color={useColorModeValue("gray.800", "white")}
+              fontWeight="bold"
+              fontSize="3xl"
+              textTransform="uppercase"
+            >
+              {title}
+            </chakra.h1>
+            <chakra.p
+              mt={1}
+              fontSize="sm"
+              color={useColorModeValue("gray.600", "gray.400")}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos
+              quidem sequi illum facere recusandae voluptatibus
+            </chakra.p>
+          </Box>
+
+          <Image h={40} w="full" fit="cover" mt={2} src={src} alt="NIKE AIR" />
+
+          <Flex
+            alignItems="center"
+            justifyContent="space-between"
+            px={4}
+            py={2}
+            bg="gray.900"
+            roundedBottom="lg"
           >
-            Button
-          </chakra.button>
-        </Flex>
-      </Box>
+            <chakra.h1 color="white" fontWeight="bold" fontSize="lg">
+              Some description
+            </chakra.h1>
+            <chakra.button
+              px={2}
+              py={1}
+              bg="white"
+              fontSize="xs"
+              color="gray.900"
+              fontWeight="bold"
+              rounded="lg"
+              textTransform="uppercase"
+              _hover={{
+                bg: "gray.200",
+              }}
+              _focus={{
+                bg: "gray.400",
+              }}
+            >
+              Button
+            </chakra.button>
+          </Flex>
+        </Box>
+      ))}
     </Flex>
   );
 };
--- src/components/wizard/Wizard.jsx
+++ src/components/wizard/Wizard.jsx
@@ -1,7 +1,13 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
+import { Box, Button, IconButton, Stack } from "@chakra-ui/react";
 import React from "react";
 import StepWizard from "react-step-wizard";
-import "./styles.css"
+import {
+  BiFirstPage,
+  BiLastPage,
+  BiChevronRight,
+  BiChevronLeft,
+} from "react-icons/bi";
+import "./styles.css";
 
 const Wizard = ({ children }) => {
   const [state, updateState] = React.useState({});
@@ -42,23 +48,41 @@ const Wizard = ({ children }) => {
 };
 
 const InstanceDemo = ({ SW, activeStep }) => (
-  <Stack className="instance-box" direction={{ base: "column", md: "row" }} spacing={5} pt={5}>
+  <Stack
+    className="instance-box"
+    direction={{ base: "column", md: "row" }}
+    spacing={5}
+    pt={5}
+  >
     {console.log("ACTIVE STEP: ", activeStep)}
     <Box>
-      <Button borderRadius={"full"} size={"sm"} onClick={SW.firstStep}>
-        First Step
-      </Button>
+      <IconButton
+        icon={<BiFirstPage />}
+        aria-label="First step"
+        onClick={SW.firstStep}
+      />
+    </Box>
+
+    <Box>
+      <IconButton
+        icon={<BiChevronLeft />}
+        aria-label="Previous step"
+        onClick={SW.previousStep}
+      />
     </Box>
     <Box>
-      <Button borderRadius={"full"} size={"sm"} onClick={SW.previousStep}>
-        {" "}
-        Previous Step
-      </Button>
+      <IconButton
+        icon={<BiChevronRight />}
+        aria-label="Previous step"
+        onClick={SW.nextStep}
+      />
     </Box>
     <Box>
-      <Button borderRadius={"full"} size={"sm"} onClick={SW.nextStep}>
-        Next Step
-      </Button>
+      <IconButton
+        icon={<BiLastPage />}
+        aria-label="Last step"
+        onClick={SW.lastStep}
+      />
     </Box>
     <Box>
       <Button
@@ -67,11 +91,6 @@ const InstanceDemo = ({ SW, activeStep }) => (
         onClick={() => SW.goToNamedStep("step3")}
       >
         Step 3
-      </Button>
-    </Box>
-    <Box>
-      <Button borderRadius={"full"} size={"sm"} onClick={SW.lastStep}>
-        Last Step
       </Button>
     </Box>
   </Stack>
